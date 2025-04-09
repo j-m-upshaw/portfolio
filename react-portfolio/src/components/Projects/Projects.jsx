bbce1ff040638d395149d8b0562f0b3681504540
j-m-upshaw <jalandmupshaw@gmail.com>
2025-04-08 22:22:29 -0500

    Completed Projects Section

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-import { getImageUrl } from "../../utils";
+import styles from "./Projects.module.css";
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
