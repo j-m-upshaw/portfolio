4883606732934567367ca28a9b8d7a007979952c
j-m-upshaw <jalandmupshaw@gmail.com>
2025-04-09 19:06:26 -0500

    added ImageCredits section

--- react-portfolio/src/components/ImageCredits/ImageCredits.jsx
+++ react-portfolio/src/components/ImageCredits/ImageCredits.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 import styles from "./ImageCredits.module.css";
+import credits from "../../data/credits.json";
 
 const ImageCredits = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h2>Images used with permission from:</h2>
+      <ul className={styles.credits}>
+        {credits.map((credit, id) => (
+          <li className={styles.credit} key={id}>
+            <a
+              href={credit.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              {credit.name}
+            </a>
+            <p>by: {credit.user}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ImageCredits;
